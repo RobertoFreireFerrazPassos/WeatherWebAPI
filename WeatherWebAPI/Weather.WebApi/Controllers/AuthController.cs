7893eb0e78829e9391ff9a4b689640c41e6ef3c7
Roberto Freire <RobertoF@99x.io>
2024-05-13 14:58:46 -0300

    - add architectural layers - add docker compose - add swagger

---
+++ WeatherWebAPI/Weather.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿namespace Weather.WebApi.Controllers;
+
+[ApiController]
+[Route("api")]
+public class AuthController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Register()
+    {
+        return Ok();
+    }
+}
