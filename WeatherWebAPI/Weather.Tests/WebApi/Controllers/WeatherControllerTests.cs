c2d8f337b823984ebf51d82faaad69d0a4b16d05
Roberto Freire <RobertoF@99x.io>
2024-05-15 20:15:28 -0300

    - add When_Is_Not_Successful_GetWeather_Should_ReturnBadRequest

--- WeatherWebAPI/Weather.Tests/WebApi/Controllers/WeatherControllerTests.cs
+++ WeatherWebAPI/Weather.Tests/WebApi/Controllers/WeatherControllerTests.cs
@@ -22,11 +22,12 @@ public class WeatherControllerTests
     public async Task GetWeather_Should_ReturnOk()
     {
         // Arrange
+        var userName = "jado47744";
         var expectedResponse = new Response<UserWeatherResponse>(true, data : new UserWeatherResponse());
         _weatherServiceMock
-            .Setup(s => s.GetWeatherAsync(It.IsAny<string>()))
+            .Setup(s => s.GetWeatherAsync(userName))
             .ReturnsAsync(expectedResponse);
-        var weatherController = GetWeatherController("jado47744");
+        var weatherController = GetWeatherController(userName);
 
         // Act
         var result = await weatherController.Weather() as ObjectResult;
@@ -41,11 +42,12 @@ public class WeatherControllerTests
     public async Task When_MissingUsername_GetWeather_Should_ReturnBadRequest()
     {
         // Arrange
+        var userName = string.Empty;
         var expectedResponse = new Response<UserWeatherResponse>(true, data: new UserWeatherResponse());
         _weatherServiceMock
-            .Setup(s => s.GetWeatherAsync(It.IsAny<string>()))
+            .Setup(s => s.GetWeatherAsync(userName))
             .ReturnsAsync(expectedResponse);
-        var weatherController = GetWeatherController(string.Empty);
+        var weatherController = GetWeatherController(userName);
 
         // Act
         var result = await weatherController.Weather() as ObjectResult;
@@ -60,10 +62,11 @@ public class WeatherControllerTests
     public async Task When_GetWeather_Throws_Exception_Returns_BadRequest()
     {
         // Arrange
+        var userName = "jado47744";
         var exceptionMessage = "An error occurred while retrieving weather data.";
-        _weatherServiceMock.Setup(x => x.GetWeatherAsync("jado47744"))
+        _weatherServiceMock.Setup(x => x.GetWeatherAsync(userName))
                           .ThrowsAsync(new Exception(exceptionMessage));
-        var weatherController = GetWeatherController("jado47744");
+        var weatherController = GetWeatherController(userName);
 
         // Act
         var result = await weatherController.Weather() as ObjectResult;
@@ -72,5 +75,26 @@ public class WeatherControllerTests
         result.Should().NotBeNull();
         result.StatusCode.Should().Be(400);
         result.Value.Should().Be(exceptionMessage);
+    }
+
+    [Fact]
+    public async Task When_Is_Not_Successful_GetWeather_Should_ReturnBadRequest()
+    {
+        // Arrange
+        var userName = "jado47744";
+        var errorMessage = "Failed to retrieve weather data.";
+        var expectedResponse = new Response<UserWeatherResponse>(false, errorMessage);
+        _weatherServiceMock
+            .Setup(s => s.GetWeatherAsync(userName))
+            .ReturnsAsync(expectedResponse);
+        var weatherController = GetWeatherController(userName);
+
+        // Act
+        var result = await weatherController.Weather() as ObjectResult;
+
+        // Assert
+        result.Should().NotBeNull();
+        result.StatusCode.Should().Be(400);
+        result.Value.Should().Be(errorMessage);
     }
 }
