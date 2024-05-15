9c3499c8b53714102e217c2f5fdd7a96b93daf83
Roberto Freire <RobertoF@99x.io>
2024-05-15 20:09:38 -0300

    - add When_GetWeather_Throws_Exception_Returns_BadRequest

--- WeatherWebAPI/Weather.Tests/WebApi/Controllers/WeatherControllerTests.cs
+++ WeatherWebAPI/Weather.Tests/WebApi/Controllers/WeatherControllerTests.cs
@@ -4,22 +4,29 @@ public class WeatherControllerTests
 {
     private readonly Mock<IWeatherService> _weatherServiceMock = new Mock<IWeatherService>();
 
-    [Fact]
-    public async Task GetWeather_Should_ReturnOk()
+    private WeatherController GetWeatherController(string userName)
     {
-        // Arrange
         var weatherController = new WeatherController(_weatherServiceMock.Object);
         weatherController.ControllerContext = new ControllerContext();
         var httpContext = new DefaultHttpContext();
         httpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
         {
-            new Claim(ClaimTypes.Name, "jado47744")
+            new Claim(ClaimTypes.Name, userName)
         }));
         weatherController.ControllerContext.HttpContext = httpContext;
+
+        return weatherController;
+    }
+
+    [Fact]
+    public async Task GetWeather_Should_ReturnOk()
+    {
+        // Arrange
         var expectedResponse = new Response<UserWeatherResponse>(true, data : new UserWeatherResponse());
         _weatherServiceMock
             .Setup(s => s.GetWeatherAsync(It.IsAny<string>()))
             .ReturnsAsync(expectedResponse);
+        var weatherController = GetWeatherController("jado47744");
 
         // Act
         var result = await weatherController.Weather() as ObjectResult;
@@ -34,18 +41,11 @@ public class WeatherControllerTests
     public async Task When_MissingUsername_GetWeather_Should_ReturnBadRequest()
     {
         // Arrange
-        var weatherController = new WeatherController(_weatherServiceMock.Object);
-        weatherController.ControllerContext = new ControllerContext();
-        var httpContext = new DefaultHttpContext();
-        httpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-        {
-            new Claim(ClaimTypes.Name, string.Empty)
-        }));
-        weatherController.ControllerContext.HttpContext = httpContext;
         var expectedResponse = new Response<UserWeatherResponse>(true, data: new UserWeatherResponse());
         _weatherServiceMock
             .Setup(s => s.GetWeatherAsync(It.IsAny<string>()))
             .ReturnsAsync(expectedResponse);
+        var weatherController = GetWeatherController(string.Empty);
 
         // Act
         var result = await weatherController.Weather() as ObjectResult;
@@ -54,5 +54,23 @@ public class WeatherControllerTests
         result.Should().NotBeNull();
         result.StatusCode.Should().Be(400);
         result.Value.Should().Be("missing username");
+    }
+
+    [Fact]
+    public async Task When_GetWeather_Throws_Exception_Returns_BadRequest()
+    {
+        // Arrange
+        var exceptionMessage = "An error occurred while retrieving weather data.";
+        _weatherServiceMock.Setup(x => x.GetWeatherAsync("jado47744"))
+                          .ThrowsAsync(new Exception(exceptionMessage));
+        var weatherController = GetWeatherController("jado47744");
+
+        // Act
+        var result = await weatherController.Weather() as ObjectResult;
+
+        // Assert
+        result.Should().NotBeNull();
+        result.StatusCode.Should().Be(400);
+        result.Value.Should().Be(exceptionMessage);
     }
 }
